48409abe6be73af8ec1e3d243f3be422054c83cb
Katelyn <katelynyewwork@gmail.com>
2026-02-25 23:50:42 -0500

    Deploy

--- shopping-cart/vite.config.js
+++ shopping-cart/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-base: "/<Shopping-cart-app>",
+base: "/Shopping-cart-app",
   plugins: [react()],
 })
